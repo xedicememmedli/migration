e4938547d83c9b3fc36084b6358a9d2fc9708fe8
Xədicə Məmmədli <memmedlix616@gmail.com>
2025-12-24 17:52:34 +0400

    Update Product.cs

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,7 +2,7 @@
 
 namespace Front_to_Back.Models
 {
-    public class Product : BaseEntity
+    public class Product
     {
         public int Id { get; set; }
         public string Title { get; set; }
